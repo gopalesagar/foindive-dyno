7d1751a3e48a3be0a8ad2d51576b81b93601e620
Narbeh Mirzaei <narmirzaei@gmail.com>
2017-10-06 18:25:10 +0330

    Limiter implementation completed

--- starter.js
+++ starter.js
@@ -4,16 +4,40 @@ const limiter = require('cpulimit');
 function run(file, params, callback) {
     var arguments = [file].concat(params)
     var command = spawn('node', arguments, {
-      stdio: 'inherit',
       shell: true
     });
 
-    var result = '';
+    command.stdout.setEncoding('utf8')
+    command.stdout.on('data', function(pid) {
+      var options = {
+          limit: 20,
+          includeChildren: true,
+          pid: pid.trim()
+      };
+
+      console.log("Limiter Options: ", options)
+
+      limiter.createProcessFamily(options, function(err, processFamily) {
+          if(err) {
+              console.error('CPU Limiter Error:', err.message);
+              return;
+          }
+
+          limiter.limit(processFamily, options, function(err) {
+              if(err) {
+                console.error('CPU Limiter Error:', err.message);
+              } else {
+                console.log('CPU Limiter Done.');
+              }
+          });
+      });
+    });
+
     command.on('close', function(code) {
-      return callback(result);
+      return callback(`Close: ${code}`);
     });
 }
 
-run("runner.js", [], function(pid) {
-  
+run("runner.js", [], function(result) {
+  console.log(`Result runner.js: ${result}`)
 });
