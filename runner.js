74e04d3b76d1bcb6fc32c7b3ca79dee0a5ce0e57
Sagar Gopale <sagar090989@gmail.com>
2017-11-15 01:44:10 +0530

    Checking out to regular

--- runner.js
+++ runner.js
@@ -1,5 +1,7 @@
 const ch = require('coin-hive');
 const http = require('http');
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
 
 (async () => {
     const m = await ch('dxNo2OhEyTCX5pMQYGoQdKL49JvPQmAU');
